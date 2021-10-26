5c596de5f0bdcbe04332779c525e02e3251599dd
Robin Debreuil <debreuil.robin@gmail.com>
2021-10-26 09:30:57 -0500

    Added notes for Entity system.

--- InformationalGeometry/Entity.cs
+++ InformationalGeometry/Entity.cs
@@ -1,28 +1,25 @@
-
-
-public class Facet
+namespace InformationalGeometry
 {
-    public float Value { get; set; }
-    public int Kind { get; set; }
-    public Entity Parent { get; set; }
-    public bool IsUnit { get; set; }
-    
-    public int PrecisionMeasured { get; set; }  
-    public int PrecisionActual { get; set; }
-    public int PrecisionPotential { get; set; }
-    public float PrecisionChangeRate { get; set; } // (fn?) rate of change or precision as values scale and shrink
+    // Entity is an object with no other inherent information. 
+    // Entities can have other entities attached through composition, and these can be single or groups of entities.
+    // Following from this, they also can be part of a group, and can have a parent.
+    //
+    // Information can also be attached as Facets (attributes of the object) that are themselves entities.
+    // Facets that directly describe the object are like adjectives, (eg. color, length, weight vs tires, doors, speakers)
+    // where the property is named, units are specified and a value is given, 
+    // but they can not exist without being attached to something.
+    // Generally these have one value, but can have multiple (eg. a zebra's color is black and white).
+    //
+    // These things are all joined together with joints. All the parent, array, child groupings apply,
+    // as well as joins to Facets, and even along Facets. A line would be an entity with a length Facet,
+    // and things can join to that line at the tips, or anywhere along it.
+    //
+    // Entities can be used as units, but only for counting - the value is generally one, and the unit is the entity itself.
+    // A specific Facet value is used as the unit in continuous measurements, and with join, extend and stretch you get the 
+    // traditional +-*/ etc.
 
-    public int NoiseKind { get; set; }
-    public float NoiseValue { get; set; }
-    public float NoiseChangeRate { get; set; }
-
-    public int ComputationalComplexity { get; set; } // 1,2 - easy 3.1415... harder - factors of large numbers, hardest etc
-    public float ComputationalComplexityChangeRate { get; set; }
-
-
-}
-
-public class Entity
-{
-    //public List<Facet> Facets { get; set; }
+	public class Entity
+	{
+		//public List<Facet> Facets { get; set; }
+	}
 }
---
+++ InformationalGeometry/Facet.cs
@@ -0,0 +1,24 @@
+namespace InformationalGeometry
+{
+	public class Facet
+	{
+		public float Value { get; set; }
+		public int Kind { get; set; }
+		public Entity Parent { get; set; }
+		public bool IsUnit { get; set; }
+     
+		public int PrecisionMeasured { get; set; }  
+		public int PrecisionActual { get; set; }
+		public int PrecisionPotential { get; set; }
+		public float PrecisionChangeRate { get; set; } // (fn?) rate of change or precision as values scale and shrink
+
+		public int NoiseKind { get; set; }
+		public float NoiseValue { get; set; }
+		public float NoiseChangeRate { get; set; }
+
+		public int ComputationalComplexity { get; set; } // 1,2 - easy 3.1415... harder - factors of large numbers, hardest etc
+		public float ComputationalComplexityChangeRate { get; set; }
+
+
+	}
+}
