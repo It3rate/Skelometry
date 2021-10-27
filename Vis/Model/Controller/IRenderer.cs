172b19025d5d91142ecfb991db0672380684d019
Robin Debreuil <it3rate@gmail.com>
2021-10-27 17:04:20 -0500

    Separated IRender class.

---
+++ Vis/Model/Controller/IRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vis.Model.Agent;
+
+namespace Vis.Model.Controller
+{
+    public interface IRenderer
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+
+        void Invalidate();
+        void SetGraphicsContext(object context);
+        void TranslateContext(float x, float y);
+        void BeginDraw(int unitPixels);
+        void Draw(IAgent agent, int penIndex = 0);
+        void EndDraw();
+    }
+}
--- Vis/Model/Controller/VisRenderer.cs
+++ Vis/Model/Controller/VisRenderer.cs
@@ -11,18 +11,6 @@ using Vis.Model.Primitives;
 
 namespace Vis.Model.Controller
 {
-    public interface IRenderer
-    {
-        int Width { get; set; }
-        int Height { get; set; }
-
-        void Invalidate();
-        void SetGraphicsContext(object context);
-        void TranslateContext(float x, float y);
-        void BeginDraw(int unitPixels);
-        void Draw(IAgent agent, int penIndex = 0);
-        void EndDraw();
-    }
 
     public class VisRenderer : Panel, IRenderer
     {
