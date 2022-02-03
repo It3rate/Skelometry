40d168a3178b54f4c6d35c27023f4e6bc4214b82
Robin Debreuil <debreuil.robin@gmail.com>
2022-02-03 13:35:40 -0600

    Added Group entity, moved selection sets to use groups.

---
+++ Slugs/Entities/Group.cs
@@ -0,0 +1,102 @@
+﻿using SkiaSharp;
+using Slugs.Pads;
+
+namespace Slugs.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Group : ElementBase
+    {
+	    public override ElementKind ElementKind => Entities.ElementKind.Group;
+	    public override IElement EmptyElement => Empty;
+	    public static readonly Group Empty = new Group();
+        public Group() : base(true) { }
+
+	    private readonly List<int> _elementKeys = new List<int>();
+	    public readonly List<SKPoint> SetPositions = new List<SKPoint>();
+
+        public Group(PadKind padKind) : base(padKind)
+	    {
+	    }
+
+        public int Count => _elementKeys.Count;
+	    public ElementKind Kind => _elementKeys.Count == 0 ? ElementKind.None : _elementKeys.Count > 1 ? ElementKind.Multiple : FirstElement.ElementKind;
+	    public IElement FirstElement => ElementAt(0);
+	    public IElement LastElement => ElementAt(_elementKeys.Count - 1);
+        public IElement ElementAt(int index) => index >= 0 && index < _elementKeys.Count ? Pad.ElementAt(_elementKeys[index]) : TerminalPoint.Empty;
+        public void Add(int elementKey)
+        {
+	        Add(Pad.ElementAt(elementKey));
+        }
+        public void Add(IElement element)
+        {
+	        _elementKeys.Add(element.Key);
+	        SetPositions.AddRange(element.SKPoints);
+        }
+        public void Add(params int[] elementKeys)
+        {
+	        foreach (var key in elementKeys)
+	        {
+		        Add(key);
+	        }
+        }
+        public void Add(IEnumerable<int> elementKeys)
+        {
+	        foreach (var key in elementKeys)
+	        {
+		        Add(key);
+	        }
+        }
+        public void AddRange(IEnumerable<IElement> elements)
+	    {
+		    foreach (var element in elements)
+		    {
+			    Add(element);
+		    }
+	    }
+	    public void Clear()
+	    {
+		    _elementKeys.Clear();
+		    SetPositions.Clear();
+	    }
+	    public bool ContainsElement(IElement element) => _elementKeys.Contains(element.Key);
+
+	    public override List<IPoint> Points
+	    {
+		    get
+		    {
+			    var pts = new List<IPoint>();
+			    foreach (var element in Elements)
+			    {
+				    pts.AddRange(element.Points);
+			    }
+			    return pts;
+		    }
+	    }
+
+        public IEnumerable<IElement> Elements
+	    {
+		    get
+		    {
+			    foreach (var element in _elementKeys)
+			    {
+				    yield return ElementAt(Key);
+			    }
+		    }
+	    }
+	    public IEnumerable<int> ElementKeys
+	    {
+		    get
+		    {
+			    foreach (var key in _elementKeys)
+			    {
+				    yield return key;
+			    }
+		    }
+	    }
+    }
+}
--- Slugs/Input/SelectionSet.cs
+++ Slugs/Input/SelectionSet.cs
@@ -25,63 +25,13 @@ namespace Slugs.Input
 	        get => _point;
 	        set { _point = value; Position = _point.Position; }
         }
-
-        private readonly List<IElement> _elements = new List<IElement>();
-        private readonly List<SKPoint> _elementPositions = new List<SKPoint>();
-        public ElementKind ElementKind => _elements.Count == 0 ? ElementKind.None : _elements.Count > 1 ? ElementKind.Multiple : _elements[0].ElementKind;
-        public IElement FirstElement => _elements.Count > 0 ? _elements[0] : TerminalPoint.Empty;
-        public void AddElement(IElement element)
-        {
-	        _elements.Add(element);
-	        _elementPositions.AddRange(element.SKPoints);
-        }
-        public void AddElements(IEnumerable<IElement> elements = null)
-        {
-	        if (elements != null)
-	        {
-		        foreach (var element in elements)
-		        {
-			        _elements.Add(element);
-			        _elementPositions.AddRange(element.SKPoints);
-		        }
-	        }
-        }
-        public void ClearElements()
-        {
-	        _elements.Clear();
-	        _elementPositions.Clear();
-        }
-        public bool ContainsElement(IElement element) => _elements.Contains(element);
-
-        public List<IPoint> ElementPoints()
-        {
-            var pts = new List<IPoint>();
-            foreach (var element in _elements)
-            {
-	            pts.AddRange(element.Points);
-            }
-            return pts;
-        }
-        public IEnumerable<IElement> Elements
-        {
-	        get
-	        {
-		        foreach (var element in _elements)
-		        {
-			        yield return element;
-		        }
-	        }
-        }
-        public IEnumerable<int> ElementKeys
-        {
-	        get
-	        {
-		        foreach (var element in _elements)
-		        {
-			        yield return element.Key;
-		        }
-	        }
-        }
+        private readonly Group _elements;
+        public IEnumerable<IElement> Elements => _elements.Elements;
+        public IEnumerable<int> ElementKeys => _elements.ElementKeys;
+        public IElement FirstElement => _elements.FirstElement;
+        public IElement LastElement => _elements.LastElement;
+        public ElementKind ElementKind => _elements.Kind;
+        public void ClearElements() => _elements.Clear();
 
         public bool HasSelection => !Point.IsEmpty || _elements.Count > 0;
         public bool HasPoint => !Point.IsEmpty;
@@ -92,6 +42,7 @@ namespace Slugs.Input
         {
 	        PadKind = padKind;
 	        SelectionSetKind = selectionSetKind;
+            _elements = new Group(padKind);
 	        Clear();
         }
 
@@ -102,27 +53,22 @@ namespace Slugs.Input
         }
         public void SetElements(params IElement[] selections)
         {
-	        ClearElements();
-	        foreach (var selection in selections)
-	        {
-				AddElement(selection);
-	        }
+	        _elements.Clear();
+            _elements.AddRange(selections);
         }
-        public void SetElements(IEnumerable<IElement> selection = null)
+        public void SetElements(IEnumerable<IElement> selection)
         {
-	        ClearElements();
-	        AddElements(selection);
+	        _elements.Clear();
+	        _elements.AddRange(selection);
         }
         public void UpdatePositions(SKPoint newPosition)
         {
 	        var dif = newPosition - Position;
-	        foreach (var element in Elements)
+	        var points = _elements.Points;
+
+            for (int i = 0; i < points.Count; i++)
 	        {
-		        var pts = element.Points;
-		        for (int i = 0; i < pts.Count; i++)
-		        {
-			        pts[i].Position = _elementPositions[i] + dif;
-		        }
+		        points[i].Position = _elements.SetPositions[i] + dif;
 	        }
             Point.Position = Position + dif; // this may or may not be in Points - maybe convert to list and always add it if not empty.
         }
@@ -130,14 +76,13 @@ namespace Slugs.Input
 	    public void Clear()
 	    {
 			Position = SKPoint.Empty;
-            //SnapPosition = Position.Empty;
             Point = TerminalPoint.Empty;
-            ClearElements();
+            _elements.Clear();
         }
 
 	    public List<IPoint> AllPoints()
 	    {
-		    var result = ElementPoints();
+		    var result = _elements.Points;
 		    if (!Point.IsEmpty)
 		    {
 			    result.Add(Point);
