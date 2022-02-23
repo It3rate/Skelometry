948d7dc100880af700aa447dddd6c8b7265abf5b
Robin Debreuil <debreuil.robin@gmail.com>
2022-02-22 21:21:26 -0600

    Added Coincident Constraint.

---
+++ Slugs/Constraints/CoincidentConstraint.cs
@@ -0,0 +1,27 @@
+﻿using Slugs.Entities;
+
+namespace Slugs.Constraints
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CoincidentConstraint : TwoElementConstraintBase
+    {
+	    public IPoint StartPoint => (IPoint)StartElement;
+	    public IPoint EndPoint => (IPoint)EndElement;
+
+        public CoincidentConstraint(IPoint startElement, IPoint endElement) : base(startElement, endElement) { }
+
+	    public override void OnStartChanged()
+	    {
+		    EndPoint.Position = StartPoint.Position;
+	    }
+	    public override void OnEndChanged()
+	    {
+		    StartPoint.Position = EndPoint.Position;
+        }
+    }
+}
